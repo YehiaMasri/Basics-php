5eeac15aa9d97d8abe7090fc1f928ca4c20ae9b6
YahyaMasri <yehiamasri83@gmail.com>
2023-02-18 13:34:42 +0200

    step-3

---
+++ Basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name='Yehia';
+$email='yehiamasri83@gmail.com';
+echo 'My name is '. $name. ' and my email is '.$email ."\n";
+$height= 175;
+$width=25;
+$area=$height*$width;
+echo "The area is:".$area;
+?>
