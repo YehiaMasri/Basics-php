65998c067bb4329f8a31ff041c9d3b0993e30f36
YahyaMasri <yehiamasri83@gmail.com>
2023-02-19 18:20:30 +0200

    step-4

---
+++ Basics-01/Step_5.php
@@ -0,0 +1,12 @@
+<?php
+$course_name="Cyber Security";
+$enrolled_students=20;
+$price=30.00;
+$on_discount= true;
+
+echo "Course title: $course_name  
+ Enrolled Studnets: $enrolled_students 
+ Course price: $price USD
+ Course on discount:";
+ echo $on_discount ? 'yes' : 'No';
+?>
