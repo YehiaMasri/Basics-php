0ec5cf859df1338eb42a6d33c590673ecaa9de53
YahyaMasri <yehiamasri83@gmail.com>
2023-02-19 18:46:29 +0200

    step-8

---
+++ Basics-01/Step_8.php
@@ -0,0 +1,16 @@
+<?php
+// Part a
+$items=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+echo "Hello Sir, do you have " .$items[0]. ", " .$items[1]. ", " .$items[2].
+ "? Also if you sell fruits can I find a " .$items[5]. "? \n";
+
+ // Part b
+
+ $groceryItems = array(
+    array('balade', 'mazere3'),
+    array ('Fresh', 'Taanayel'),
+    array( 'Tanoureen', 'Reem')
+);
+echo "Hey Sir, Please I need 1 pack of {$groceryItems[0][0]} {$items[0]} and 3 {$groceryItems[2][1]} {$items [3]}.\n"
+
+?>
